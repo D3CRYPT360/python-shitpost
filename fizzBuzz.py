689fa14d42848ae3a7e191ba87e120d34677a1fb
D3crypt360 <70775520+D3CRYPT360@users.noreply.github.com>
2021-01-30 19:19:35 +0500

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -8,7 +8,12 @@ if number is divisible by both 5 and 3 replace number with FizzBuzz
 """
 
 for i in range(1,31): # cuz why not lol
-    if i%5 == 0 and i%3 == 0: 
+    if i%5 == 0 and i%3 == 0:
+        """
+        putting it here because if a number is divisible by 3 or 5 and if
+        i put `i%x` == 0 here instead of this check it would take that and
+        this check will never happen. - note to self, not interesting.
+        """
         r.append("FizzBuzz")
     elif i%3 == 0:
         r.append("Fizz")
